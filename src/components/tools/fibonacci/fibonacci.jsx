27fe3fb3a73836ef99d02776b3e9be1196a90da9
amirreza <notname1957@gmail.com>
2023-01-24 12:21:09 +0330

    fix fibonachi problems

--- src/components/tools/fibonacci/fibonacci.jsx
+++ src/components/tools/fibonacci/fibonacci.jsx
@@ -12,11 +12,11 @@ export default function Fibonacci() {
 
   const calculate = () => {
     console.log('Fibonacci Series:');
-    let n1 = 0,
-      n2 = 1,
-      nextTerm;
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
 
-    for (let i = 1; i <= givenNumber; i++) {
+    for (let i = 1; i <= givenNumber; i += 1) {
       nextTerm = n1 + n2;
       n1 = n2;
       n2 = nextTerm;
