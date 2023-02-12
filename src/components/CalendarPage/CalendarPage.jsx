31236200298dc2e8ff116567c7f6bd1db90cc927
amirreza <notname1957@gmail.com>
2023-02-12 23:02:50 +0330

    added calendar

--- src/components/CalendarPage/CalendarPage.jsx
+++ src/components/CalendarPage/CalendarPage.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-// import { Calendar } from 'react-persian-datepicker';
+import JCalendar from 'reactjs-persian-calendar';
+import { useSelector } from 'react-redux';
+import { css } from '@emotion/css';
 
 function CalendarPage() {
-  return <div>{/* <Calendar /> */}</div>;
+  const { localeLanguage } = useSelector((state) => state.data);
+
+  return (
+    <div
+      className={css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+      `}
+    >
+      <JCalendar
+        locale={localeLanguage === 'en' ? 'en' : 'fa'}
+        color="#000066"
+        size={54}
+        onClick={console.log}
+        itemRender={(key, item, children) => children}
+      />
+    </div>
+  );
 }
 
 export default CalendarPage;
