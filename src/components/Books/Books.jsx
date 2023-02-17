da1afba5a4f4a4ab8a430382c4583782346b9394
amirreza <notname1957@gmail.com>
2023-02-17 17:00:58 +0330

    added backend and fixed books

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Books() {
-  return <div>Books</div>;
+  return (
+    <div>
+      <a href="http://127.0.0.1:5173/src/assets/pdf/riyazitakmili.pdf">
+        riyaziiii
+      </a>
+    </div>
+  );
 }
-
 export default Books;
